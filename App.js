1f9b6b3fdd31e26254deb3d78fe3edc25c9bafac
seungho Jeon <urijsh612@gmail.com>
2019-12-12 19:39:28 +0900

    #10.1 Preloading Assets

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { Text, View } from "react-native";
+import { AppLoading } from "expo"; // 로딩 작업을 실시하는 컴포넌트
+import * as Font from "expo-font";
+import { Asset } from "expo-asset";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+  const [loaded, setLoaded] = useState(false);
+  const preLoad = async () => {
+    try {
+      await Font.loadAsync({
+        //font를 preload
+        ...Ionicons.font
+      });
+      await Asset.loadAsync([require("./assets/instaLogo.jpeg")]);
+      setLoaded(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    preLoad();
+  }, []);
+  return loaded ? (
+    <View>
+      <Text>앱이 실행 되었습니다</Text>
     </View>
+  ) : (
+    <AppLoading />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
