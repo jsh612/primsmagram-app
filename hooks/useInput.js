1e7bcb314e5330e0cbd8c4e7531f94a3ff0b2dbc
seungho Jeon <urijsh612@gmail.com>
2019-12-20 00:02:51 +0900

    #12.9 Facebook Login part Two

--- hooks/useInput.js
+++ hooks/useInput.js
@@ -6,7 +6,7 @@ const useInput = initValue => {
     //text --> 일반적인 event.target.value 와 같은 값
     setValue(text);
   };
-  return { value, onChange };
+  return { value, onChange, setValue }; // setValue로 value 값 변경시 해당 페이지 리랜더 된다.
 };
 
 export default useInput;
