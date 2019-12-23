7302dca3057e7c116a06bc48d13c56a8c5e74704
seungho Jeon <urijsh612@gmail.com>
2019-12-23 14:07:48 +0900

    #14.0 Apollo Context

--- apollo.js
+++ apollo.js
@@ -1,6 +1,17 @@
+import { AsyncStorage } from "react-native";
+
 // ApolloClient 옵션 설정
 const apolloClientOptions = {
-  uri: "http://localhost:4000"
+  uri: "http://localhost:4000",
+  request: async operation => {
+    //매 요청마다 토큰을 header에 전달하여 유저 인증이 되었음을 보여준다
+    const token = await AsyncStorage.getItem("jwt");
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ""
+      }
+    });
+  }
 };
 
 export default apolloClientOptions;
--- screens/Tabs/Home.js
+++ screens/Tabs/Home.js
@@ -1,6 +1,38 @@
 import React from "react";
 import styled from "styled-components";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 import Loader from "../../components/Loader";
+
+const FEED_QUERY = gql`
+  {
+    seeFeed {
+      id
+      location
+      caption
+      user {
+        id
+        avatar
+        username
+      }
+      files {
+        id
+        url
+      }
+      likeCount
+      isLiked
+      comments {
+        id
+        text
+        user {
+          id
+          username
+        }
+      }
+      createdAt
+    }
+  }
+`;
 
 const View = styled.View`
   justify-content: center;
@@ -8,8 +40,7 @@ const View = styled.View`
   flex: 1;
 `;
 
-export default () => (
-  <View>
-    <Loader />
-  </View>
-);
+export default () => {
+  const { loading, data } = useQuery(FEED_QUERY);
+  return <View>{loading ? <Loader /> : null}</View>;
+};
