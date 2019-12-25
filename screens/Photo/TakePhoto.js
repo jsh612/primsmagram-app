cb84a221afd5bd2d7c6cceed1e9f6c6bd729e3d5
seungho Jeon <urijsh612@gmail.com>
2019-12-25 22:02:28 +0900

    #18.4 Take Photo part Two

--- screens/Photo/TakePhoto.js
+++ screens/Photo/TakePhoto.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, Platform } from "react-native";
 import styled from "styled-components";
 import { Camera } from "expo-camera";
+import { Ionicons } from "@expo/vector-icons";
 import Loader from "../../components/Loader";
 import constants from "../../constants";
 
@@ -9,9 +10,12 @@ const View = styled.View`
   flex: 1;
 `;
 
+const Icon = styled.View``;
+
 export default ({ navigation }) => {
   const [loading, setLoading] = useState(true);
   const [hasPermisson, setHasPermisson] = useState(false);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.front);
 
   const askPermission = async () => {
     // 카메라 접근 권한 받기
@@ -28,6 +32,15 @@ export default ({ navigation }) => {
     }
   };
 
+  const toggleType = () => {
+    // 카메라 전,후면 전환
+    if (cameraType === Camera.Constants.Type.front) {
+      setCameraType(Camera.Constants.Type.back);
+    } else {
+      setCameraType(Camera.Constants.Type.front);
+    }
+  };
+
   useEffect(() => {
     askPermission();
   }, []);
@@ -38,8 +51,28 @@ export default ({ navigation }) => {
         <Loader />
       ) : hasPermisson ? (
         <Camera
-          style={{ width: constants.width, height: constants.height / 2 }}
-        />
+          style={{
+            width: constants.width,
+            height: constants.height / 2,
+            justifyContent: "flex-end",
+            padding: 15
+          }}
+          type={cameraType}
+        >
+          <TouchableOpacity onPress={toggleType}>
+            <Icon>
+              <Ionicons
+                name={
+                  Platform.OS === "ios"
+                    ? "ios-reverse-camera"
+                    : "md-reverse-camera"
+                }
+                size={32}
+                color={"white"}
+              />
+            </Icon>
+          </TouchableOpacity>
+        </Camera>
       ) : null}
     </View>
   );
