51af564c1eb0f381c98f98b6fc7ac499c2a6d903
seungho Jeon <urijsh612@gmail.com>
2019-12-27 10:23:59 +0900

    로그인 임시 코드 수정

--- components/NavController.js
+++ components/NavController.js
@@ -5,6 +5,5 @@ import MainNavigation from "../navigation/MainNavigation";
 
 export default () => {
   const isLoggedIn = useIsLoggedIn();
-  // return isLoggedIn ? <MainNavigation /> : <AuthNavigation />; // 잠시 주석처리
-  return <MainNavigation />; // 임시 코드
+  return isLoggedIn ? <MainNavigation /> : <AuthNavigation />;
 };
